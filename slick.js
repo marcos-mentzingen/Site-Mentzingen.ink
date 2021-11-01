8d3e1495893bf91eda777060168ac3d329882c13
Marcos Mentzingen <marcosmentzingen@edu.unirio.br>
2021-11-01 06:04:06 -0300

    Criação do Menu e introdução do Slick
    Comecei a criar o site
    - Introdução da logo e Menu
    - Introdução de um slide, utilizando Slick

---
+++ slick.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider-principal').slick({
+        // slide:'li',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: false,
+        arrows: false,
+        dots: true,
+        
+    });
+  });
